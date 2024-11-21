25deebde2924d3a46ac6d7331206d7093afe3c84
Souvik Sen <88578056+Souvik0901@users.noreply.github.com>
2024-11-21 12:25:49 +0530

    Update sendSMS.js

--- sendSMS.js
+++ sendSMS.js
@@ -1,12 +1,10 @@
-
 // on preview SMS List and Template list are showing
-
 var $ = skuid.$;
 var templateList = skuid.model.getModel('Templates');
 var applicationList = skuid.model.getModel('Applications'); 
 var smsList = skuid.model.getModel('SMSModel'); 
 
-// Get the first record from the Applications model
+
 var app = applicationList.data[0]; 
 var appId = app.Id; 
 
@@ -14,25 +12,30 @@ var template;
 var smsType;
 var body;
 
-for(let i=0; i<3; i++){  
-    template = templateList.data[i];
-    smsType = templateList.data[i].Template_Type__c;   
 
+for(let i=0;i<3;i++){
+    if(templateList.data[i].isUpdated === 'Yes'){
+        template = templateList.data[i];
+    }
 }
 
 
 body = template.Template_Text__c; 
 console.log(body);
 
-// Insert the new SMS record into the SMSModel
 smsList.updateRow(smsList.createRow(), {
     genesis__Application__c:appId, 
+    Application__c: appId,
     Body__c: body,
     From__c: 'Latitude',  
     To__c: app.Mobile_Number__c, 
-    Template_Type__c:smsType,  
-    isUpdated: 'No', 
 });
+
+
+
+
+You need to change on actions also hiting the button:
+
 
 
 // console.log(smsRecord);
