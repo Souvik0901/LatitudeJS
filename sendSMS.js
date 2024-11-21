ef8753edc86d4f4678f929bcb80eab4ce9e00e5d
Souvik Sen <88578056+Souvik0901@users.noreply.github.com>
2024-11-21 13:52:04 +0530

    Update sendSMS.js

--- sendSMS.js
+++ sendSMS.js
@@ -1,46 +1,34 @@
+// on preview SMS List and Template list are showing
 var $ = skuid.$;
 var templateList = skuid.model.getModel('Templates');
 var applicationList = skuid.model.getModel('Applications'); 
 var smsList = skuid.model.getModel('SMSModel'); 
 
-// Get the first record from the Applications model
+console.log(applicationList);
 var app = applicationList.data[0]; 
+console.log(app);
 var appId = app.Id; 
 
 var template;
-var smsType; 
+var smsType;
+var body;
 
-// Check if there is a template available
-if (templateList.data.length > 0) {
-    template = templateList.data[0]; 
-    smsType = template.Template_Type__c; 
-    var body = template.Template_Text__c; 
 
-    // Prepare a new SMS record to insert into SMSModel
-    var smsRecord = {
-        Body__c: body, 
-        From__c: 'Latitude',  
-        To__c: app.Mobile_Number__c, 
-        Template_Type__c: smsType,  
-        isUpdated: 'No', 
-    };
-    
-    // Insert the new SMS record into the SMSModel
-    smsList.updateRow(smsList.createRow(), {
-        Body__c: body, 
-        From__c: 'Latitude',  
-        To__c: app.Mobile_Number__c, 
-        Template_Type__c: smsType,  
-        isUpdated: 'No', 
-    });
-
-    
-    console.log(smsRecord);
-    console.log(smsList.data[0]);
-
-    
-    skuid.ui.showMessage('Twilio SMS record inserted successfully!');
-} else {
-   
-    skuid.ui.showMessage('No templates found for sending SMS!');
+for(let i=0;i<templateList.data.length;i++){
+    if(templateList.data[i].isUpdated === 'Yes'){
+        template = templateList.data[i];
+    }
 }
+
+
+body = template.Template_Text__c; 
+console.log(body);
+
+smsList.updateRow(smsList.createRow(), {
+    genesis__Application__c:appId, 
+    Application__c: appId,
+    Body__c: body,
+    From__c: 'Latitude',  
+    To__c: app.Mobile_Number__c, 
+});
+
