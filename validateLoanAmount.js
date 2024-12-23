6f4c332e2653fd10fc2a056714899930a9738097
Souvik Sen <88578056+Souvik0901@users.noreply.github.com>
2024-12-23 13:36:48 +0530

    Update validateLoanAmount.js

--- validateLoanAmount.js
+++ validateLoanAmount.js
@@ -39,9 +39,28 @@ if((applicationData.Top_Up__c === true )
         return displayMessage('The total loan amount (excluding the Loan Establishment Fee) must be at least $5,000. Please adjust the top-up amount if you wish to proceed.', 'ERROR');
 }
 
+///**************************************** For LMAU-3278 ******************************************///
 
+if(applicationData.genesis__Loan_Amount__c <=5000 && applicationData.broker_fee__c >0){
+    return displayMessage('The Broker Fee Cannot exceed $0 for loan amounts $4999 or less', 'ERROR');
+}
 
-/////////////// FOR LMAU-3278 //////////////////
-if(applicationData.genesis__Loan_Amount__c >0 && applicationData.broker_fee__c >650){
-    return displayMessage('Hereeee we go', 'ERROR');
+if((applicationData.genesis__Loan_Amount__c <=7500 && applicationData.genesis__Loan_Amount__c >5000) && applicationData.broker_fee__c >200){
+    return displayMessage('The Broker fee cannot exceed $200 for loan amounts between $5000-$7,499.99', 'ERROR');
+}
+
+if((applicationData.genesis__Loan_Amount__c <=10000 && applicationData.genesis__Loan_Amount__c >7500) && applicationData.broker_fee__c >650){
+    return displayMessage('The Broker fee cannot exceed $650 for loan amounts between $7500- $9999.99', 'ERROR');
+}
+
+if((applicationData.genesis__Loan_Amount__c <=20000 && applicationData.genesis__Loan_Amount__c >10000) && applicationData.broker_fee__c >990){
+    return displayMessage('The Broker fee cannot exceed $990 for loan amounts between $10,000 - $19,999.99', 'ERROR');
+}
+
+if((applicationData.genesis__Loan_Amount__c <=50000 && applicationData.genesis__Loan_Amount__c >20000) && applicationData.broker_fee__c >2200){
+    return displayMessage('The Broker fee cannot exceed $2200 for loan amounts between $20,000 - $49,999.99', 'ERROR');
+}
+
+if(applicationData.genesis__Loan_Amount__c >=50000 && applicationData.broker_fee__c >2500){
+    return displayMessage('The Broker fee cannot exceed $2500 for Loan amounts greater than or equal to $50,000', 'ERROR');
 }
